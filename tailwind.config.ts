ab750619edfd8a75e584a4289086d7e5a501f37d
agrim08 <agrimgupta0805@gmail.com>
2026-03-27 05:16:56 +0530

    color pallet

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        sans: ["var(--font-inter)", ...fontFamily.sans],
       },
       borderRadius: {
         lg: "var(--radius)",
